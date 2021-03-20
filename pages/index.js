aca52a9e4128ef970ab50394e9398fb4c0bb0cc6
SkylineCodes <onakoyak@gmail.com>
2021-03-20 18:13:30 +0100

    Added viewport header to index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Head>
         <title>Mizright</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <Hero />
       {/* <Feature /> */}
