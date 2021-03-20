b5b76f12acc84ea7972530f0f0626689471b9271
SkylineCodes <onakoyak@gmail.com>
2021-03-20 18:26:33 +0100

    Added viewport header to layout

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       <Head>
         <title>Mizright</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       </Head>
       <Hero />
       {/* <Feature /> */}
